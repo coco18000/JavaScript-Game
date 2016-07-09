c82dae309b7bb6e99ad39ad39aa4ae8c1ff7e194
Katsuya Kikuchi <kikuchi.k.0711@gmail.com>
2016-07-09 23:15:53 +0900

    up

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
+window.addEventListener('load',init) ;
+
 //------------------------------------------------//
 //------------------------------------------------//
-var SCREEN_SIZE = 500; // キャンバスの幅
+var SCREEN_HEIGHT = 600;	// キャンバスの高さ
+var SCREEN_WIDTH = 800;		// キャンパスの幅
 var canvas; //= document.getElementById('world');
 var context; //= canvas.getContext('2d');
 //------------------------------------------------//
 //------------------------------------------------//
 
 //index.htmlが読み込まれた際に行われる初期化処理
-window.onload = function() {
+function init() {
     canvas = document.getElementById('GameWindow'); // index.htmlからcanvas要素を取得
-    canvas.width = canvas.height = SCREEN_SIZE; // キャンバスのサイズを設定
     context = canvas.getContext('2d');                // コンテキスト
+
+ 	canvas.width = SCREEN_WIDTH;
+ 	canvas.height = SCREEN_HEIGHT;
+
+	requestAnimationFrame(update);
 }
 
 //ゲーム画面の更新
 function update() {
+	requestAnimationFrame(update);
+	render();
 }
 
 //更新したゲーム画面の再描写
-function draw() {
+function render() {
+	context.clearRect(0, 0, canvas.width, canvas.height);
 }
